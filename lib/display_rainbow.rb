968a03d7987819aaa64adcf58b2ec8198cd955e1
darnell ghee <jasmine.blackmon@gmail.com>
2018-08-11 17:36:19 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,10 @@
 # Write your #display_rainbow method here
+
+# We dont have to assign the values to color_array.
+# The test will do that in the background for you.
+# We know that it will look like this:
+# {'red', 'orange', 'yellow', 'green', 'blue', 'indigo','violet'}
+
+def display_rainbow(color_array)
+ puts "R: #{color_array[0]}, O: #{color_array[1]}, Y: #{color_array[2]}, G: #{color_array[3]}, B: #{color_array[4]}, I: #{color_array[5]}, V: #{color_array[6]}" 
+ end
